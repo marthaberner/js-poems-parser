2e8b87cac0ed8aa9d39bf86cf43cfb9305f3b39a
marthaberner <marthaberner.mb@gmail.com>
2016-05-18 21:43:42 -0600

    wire up mocha chai

---
+++ test/poems.test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('Poems', function() {
+  describe('set up', function () {
+    it('is wired up', function () {
+      assert.equal(true, true);
+    });
+  });
+});
